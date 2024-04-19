141195cef6f25a3301a86e52429039cc173b9987
reskeee <150681013+reskeee@users.noreply.github.com>
2024-04-19 16:39:42 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,84 +1,58 @@
-import cv2
-import numpy as np
-from ultralytics import YOLO
-from help import get_coords
-from sympy import Point, Polygon
+from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog
+from PyQt5 import uic
+import sys
+from main1 import predict
+from colorama import Back
 
-model = YOLO('yolov8n')
 
-cap = cv2.VideoCapture('test.mp4')
-# cap = cv2.VideoCapture('PXL_20231205_031521968.TS~2_new.mp4')
-# cap = cv2.VideoCapture('PXL_20231202_044440610.TS~2_CUT.mp4')
-# cap = cv2.VideoCapture('PXL_20231202_044440610.TS~2.mp4')
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("UI/main.ui", self)
 
-# Получение координат четырехугольника из файла
-tetragons = get_coords('poly_vertex1.txt')
-# Преобразование: список(кортеж(число, число)) -> список(Polygon(Point2D, Point2D))
-drawed_tetragons = list(map(lambda x: Polygon(*x), tetragons))
+        self.startButton.clicked.connect(self.start_prediction)
+        self.changeButton.clicked.connect(self.choose_video)
+        self.zoneButton.clicked.connect(self.choose_zones)
 
-# Задаю цвета
-RED = (0, 0, 255)
-GREEN = (0, 255, 0)
+        self.video_filename = None
+        self.zones_filename = None
 
-# Перебор кадров
-while cap.isOpened():
-    # Чтение кадра
-    success, frame = cap.read()
+    def start_prediction(self):
+        print(Back.GREEN + "start_click")
+        print(Back.GREEN + str(self.video_filename))
+        print(Back.GREEN + str(self.zones_filename))
+        if self.video_filename and self.zones_filename:
+            predict(self.video_filename, self.zones_filename)
+        else:
+            if not self.video_filename:
+                self.label_2.setStyleSheet('background-color: red;')
+            if not self.video_filename:
+                self.label.setStyleSheet('background-color: red;')
 
-    # Если кадр есть
-    if success:
-        # Результат обработки кадра
-        results = list(model.track(frame, persist=True, imgsz=320))
-        annotated_frame = frame
+    def choose_video(self):
+        print("change_file")
+        file = QFileDialog.getOpenFileName(
+            self, "Выберите видео для обработки", '', "*.mp4;;*.avi")[0]
+        self.label.setText(file)
+        self.video_filename = file
+        self.label.setStyleSheet('background-color: white;')
 
-        # Создание "запрещенных четырехугольников"
-        for tetragon in tetragons:
-            draw = np.array(tetragon, np.int32)
-            cv2.polylines(annotated_frame, [draw], True, (0, 255, 255), 3, lineType=3)
-            cv2.line(annotated_frame, draw[1], draw[3], (0, 255, 255), 3)
-            cv2.line(annotated_frame, draw[0], draw[2], (0, 255, 255), 3)
+    def choose_zones(self):
+        print("zones")
+        file = QFileDialog.getOpenFileName(
+            self, "Выберите описания зон", '', "*.txt")[0]
+        self.label_2.setText(file)
+        self.zones_filename = file
+        self.label_2.setStyleSheet('background-color: white;')
 
-        # Узнаю размер окна (высота, ширина)
-        orig_shape = results[0].orig_shape
-        # Все определенные объекты
-        res = results[0].boxes.xyxyn.tolist()
+# def except_hook(cls, exception, traceback):
+#     sys.excepthook(cls, exception, traceback)
 
-        print(f'Все объекты: {res}')
 
-        # Если на кадре обнаружены объекты
-        if res:
-            for i in range(len(res)):  # Для каждого объекта
-                # Координаты углов коробки
-                box_cords = res[i]
-                box = [(int(box_cords[0] * orig_shape[1]), int(box_cords[1] * orig_shape[0])),
-                       (int(box_cords[2] * orig_shape[1]), int(box_cords[1] * orig_shape[0])),
-                       (int(box_cords[2] * orig_shape[1]), int(box_cords[3] * orig_shape[0])),
-                       (int(box_cords[0] * orig_shape[1]), int(box_cords[3] * orig_shape[0]))]
-
-                print(f'Коробка нынешнего объекта{box_cords}')
-
-                # Координата точки ног объекта
-                centre_coord = (int(((box_cords[0] + box_cords[2]) / 2) * orig_shape[1]),
-                                int((box_cords[3]) * orig_shape[0]))
-                # Отображение точки
-                cv2.circle(annotated_frame, centre_coord, 6, (255, 0, 0), -1)
-
-                # Отображение рамок объектов
-                for polygon in drawed_tetragons:  # Для каждой рамки
-                    # Если точка внутри четырехугольника
-                    if polygon.encloses_point(Point(centre_coord[0], centre_coord[1])):
-                        cv2.putText(annotated_frame, "Danger!", (50, 900), cv2.FONT_HERSHEY_SIMPLEX,
-                                    5, (0, 0, 255), 2)
-                        cv2.rectangle(annotated_frame, box[0], box[2], RED, 4)
-
-        cv2.imshow("YOLOv8 Inference", annotated_frame)
-
-        # break
-
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
-    else:
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    app.setStyle('Fusion')
+    main_window = MainWindow()
+    main_window.show()
+    # sys.excepthook = except_hook
+    sys.exit(app.exec())
