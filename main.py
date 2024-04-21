e1c8b6a8db308881c1a6c28c3d90f53813fda35c
reskeee <150681013+reskeee@users.noreply.github.com>
2024-04-21 13:44:54 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog
 from PyQt5 import uic
 import sys
-from main1 import predict
+from prediction import predict
 from colorama import Back
 
 
